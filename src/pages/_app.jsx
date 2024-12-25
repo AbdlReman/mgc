8ce4a3d5af32bd2d247b30bcbbe93542fbd7116d
Bilal Ahmad <bilalahmadkhan1729@gmail.com>
2024-12-26 00:51:19 +0500

    gtag

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,13 +1,28 @@
-
 import "@/src/styles/index.scss";
-
-
 
 if (typeof window !== "undefined") {
   require("bootstrap/dist/js/bootstrap");
 }
 
-
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* Google tag (gtag.js) */}
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-YVRH0YTT6P"
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-YVRH0YTT6P');
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
